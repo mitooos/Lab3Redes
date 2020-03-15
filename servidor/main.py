853a3128351a83cd5d76c32170dd6f235e01515e
Diego Chacon <ds.chacon@uniandes.edu.co>
2020-03-15 10:27:05 -0500

    archivo log

--- servidor/main.py
+++ servidor/main.py
@@ -21,15 +21,23 @@ arch1 = 'arch1.txt'
 
 arch2 = 'arch2.mp4'
 
+clientes = []
+
 #funcion que corre en paralelo para interactuar con los clientes
 def thread(conn, i):
     ehlo = conn.recv(1024).decode('ascii')
     print('El cliente', i, 'se conecto')
     print(ehlo)
 
+    log = open ('log.txt','wa')
+    log.write(str(time.time()))
+    
+
     conn.send('Que archivo desea?'.encode('ascii'))
 
     numero_archivo = int.from_bytes(conn.recv(1),'big')
+
+    
 
     #espera a que todos los clientes esten listos para enviar el archivo
     while True:
@@ -45,15 +53,19 @@ def thread(conn, i):
         conn.send(len(arch1).to_bytes(4,'big'))
         conn.send(arch1.encode('ascii')) #envia nombre del archivo
         conn.send(os.path.getsize(arch1).to_bytes(8,'big')) # envia tamaño del archivo
+        log.write('\n'+str(arch1)+'-'+str(os.path.getsize(arch1)))
         f = open(arch1, 'rb')
 
     if numero_archivo == 2:
         conn.send(len(arch2).to_bytes(4,'big'))
         conn.send(arch2.encode('ascii')) #envia nombre del archivo
         conn.send(os.path.getsize(arch2).to_bytes(8,'big')) # envia tamaño del archivo
+        log.write('\n'+str(arch2)+'-'+str(os.path.getsize(arch2)))
         f = open(arch2, 'rb')
     
     conn.recv(1024)
+
+    
 
     # objeto hash
     h = hashlib.sha1()
@@ -75,9 +87,13 @@ def thread(conn, i):
     conn.send(hash_archivo.encode('ascii'))
     print('se envio el hash del archivo al cliente: ', i)
     print(hash_archivo)
-
+    
+    log.write('\n'+clientes[i]+'//'+str(tiempo_final)+' seg')
     
     print("El tiempo transcurrido fue: "+ str(tiempo_final)+" segundos")
+    
+    log.close()
+
     conn.close()
 
 
@@ -92,6 +108,7 @@ def receive_connections():
         c, addr = s.accept()
         conexiones += 1
         print('Conectado con el cliente en: ', addr[0], ':', addr[1])
+        clientes.append(str(conexiones)+'/'+str(addr[0]))
         start_new_thread(thread, (c,conexiones))
 
         #avisa que ya hay la cantidad de clientes necesarios para enviar el archivo
